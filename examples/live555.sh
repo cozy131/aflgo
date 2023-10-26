c42ff0f930b1206c724c09809f2014862055da6c
cozy131 <74371106+cozy131@users.noreply.github.com>
2023-10-26 21:12:51 +0800

    Update live555.sh

--- examples/live555.sh
+++ examples/live555.sh
@@ -8,7 +8,7 @@ mkdir tmp
 export SUBJECT=$PWD; export TMP_DIR=$PWD/tmp
 
 # changing for your environment
-export AFLGO=$HOME/fuzzingtool/SnapFuzz-artefact/builds/aflgo
+export AFLGO=$HOME/fuzzingtool/snap-aflgo/builds/aflgo
 
 # changing for your environment
 echo "MPEG1or2Demux.cpp:129" > $TMP_DIR/BBtargets.txt
@@ -18,11 +18,12 @@ make -j clean all
 cat $TMP_DIR/BBnames.txt | rev | cut -d: -f2- | rev | sort | uniq > $TMP_DIR/BBnames2.txt && mv $TMP_DIR/BBnames2.txt $TMP_DIR/BBnames.txt
 cat $TMP_DIR/BBcalls.txt | sort | uniq > $TMP_DIR/BBcalls2.txt && mv $TMP_DIR/BBcalls2.txt $TMP_DIR/BBcalls.txt
 
-$AFLGO/distance/gen_distance_fast.py ./testProgs/ $TMP_DIR 
+$AFLGO/distance/gen_distance_fast.py $SUBJECT/testProgs/ $TMP_DIR  
 
 ./genMakefiles linux2
 make -j clean all
 
 # changing for your environment
-export SNAPFUZZ=$HOME/fuzzingtool/SnapFuzz-artefact
+export SNAPFUZZ=$HOME/fuzzingtool/snap-aflgo
+
 cp ./testProgs/testOnDemandRTSPServer "${SNAPFUZZ}/testOnDemandRTSPServer"
